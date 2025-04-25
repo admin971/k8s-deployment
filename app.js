b3b7b1e27ab7f43f97aa9876c8e1017ae79c1a0b
root <root@ip-172-31-0-202.ap-southeast-2.compute.internal>
2025-04-25 07:13:34 +0000

    sdfsd

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js on Kubernetes!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
